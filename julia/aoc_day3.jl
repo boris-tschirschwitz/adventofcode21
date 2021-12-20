19e19fa795774d83a61b8b1d8551598e4314b05b
Boris Tschirschwitz <boris.tschirschwitz@futurice.com>
2021-12-20 09:29:26 +0100

    day 3: add type annotations

--- julia/aoc_day3.jl
+++ julia/aoc_day3.jl
@@ -2,7 +2,7 @@
 
 include("aoc_day3_data.jl")
 
-function parseData(data)
+function parseData(data::String)::Tuple{Vector{Int}, Int}
     function splitLine(line)
         map(n -> parse(Int, n), collect(line))
     end
@@ -11,7 +11,7 @@ function parseData(data)
     (map(l -> parse(Int, l, base = 2), lines), bits)
 end
 
-function populations(numbers, bits)
+function populations(numbers::Vector{Int}, bits::Int)::Vector{Int}
     counts = fill(0, bits)
     for n in numbers
         for i = 0:(bits-1)
@@ -21,24 +21,24 @@ function populations(numbers, bits)
     return counts
 end
 
-function rate(test, populations)
+function rate(test::Function, populations::Vector{Int})::Int
     reduce((total, bit) -> total * 2 + bit, map(test, reverse(populations)), init = 0)
 end
 
-function gammaRate(populations, count)
-    rate(p -> p > (count - p), populations)
+function gammaRate(populations::Vector{Int}, bits::Int)::Int
+    rate(p -> p > (bits - p), populations)
 end
 
-function epsilonRate(populations, count)
-    rate(p -> p < (count - p), populations)
+function epsilonRate(populations::Vector{Int}, bits::Int)::Int
+    rate(p -> p < (bits - p), populations)
 end
 
-function filterBit(test, numbers, bit)
+function filterBit(test::Function, numbers::Vector{Int}, bit::Int)
     numberOfOnes = sum(map(x -> (x >> bit) % 2, numbers))
     test(numberOfOnes, length(numbers)) ? 1 : 0
 end
 
-function eleminationRating(bitCriteria, numbers, bits)
+function eleminationRating(bitCriteria::Function, numbers::Vector{Int}, bits::Int)::Int
     remaining = numbers
     for i = (bits-1):-1:0
         if length(remaining) < 2
@@ -50,11 +50,11 @@ function eleminationRating(bitCriteria, numbers, bits)
     return remaining[1]
 end
 
-function oxygenGeneratorRating(numbers, bits)
+function oxygenGeneratorRating(numbers::Vector{Int}, bits::Int)::Int
     eleminationRating((pop, len) -> pop >= len - pop, numbers, bits)
 end
 
-function co2ScrubberRating(numbers, bits)
+function co2ScrubberRating(numbers::Vector{Int}, bits::Int)::Int
     eleminationRating((pop, len) -> pop < len - pop, numbers, bits)
 end
 
